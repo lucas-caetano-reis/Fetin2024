2169484aa9ec6e69bfccaeccaf61baac3611114d
Lucas Caetano Reis <108757739+lucas-caetano-reis@users.noreply.github.com>
2024-05-06 20:42:14 -0300

    Alexandria Arquivos
    Todos os arquivos já criados. Atualmente trabalhando na tela de pesquisa/filtragem/listagem

---
+++ Alexandria/Alexandria/listagem/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+# Register your models here.
+from listagem.models import Autor, Livro
+
+class AutorAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Nome do autor', {'fields': ['autor_nome']}),
+    ]
+
+class LivroAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Nome do livro', {'fields': ['livro_nome']}),
+        ('Série a qual o livro pertence', {'fields': ['livro_serie'], 'classes': ['collapse']}),
+        ('Autor do livro', {'fields': ['livro_autor']}),
+        ('Gênero do livro', {'fields': ['livro_genero']}),
+    ]
+    list_display = ('livro_nome', 'livro_serie', 'livro_autor', 'livro_genero')
+    list_filter = ['livro_autor', 'livro_genero', 'livro_serie']
+    search_fields = ['livro_nome']
+
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Livro, LivroAdmin)
---
+++ Alexandria/Alexandria/listagem/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Autor(models.Model): #tabela dos autores
+    autor_id = models.AutoField(primary_key=True) #chave primária do autor
+    autor_nome = models.CharField(max_length = 200)
+
+    class Meta:
+        ordering = ['autor_nome'] #coloca os autores em ordem de nome
+
+    def __str__(self):
+        return self.autor_nome #retorna o atributo nome
+
+class Livro(models.Model): #tabela dos livros
+    livro_id = models.AutoField(primary_key=True) #chave primária do livro
+    livro_nome = models.CharField(max_length = 200)
+    livro_serie = models.CharField(max_length = 200 , blank=True) #blank: permite texto vazio
+    livro_autor = models.ForeignKey(Autor, models.CASCADE, related_name="autor") #relacionamento entre livro e autor: um autor pode escrever vários livros
+    livro_genero = models.CharField(max_length = 200)
+
+    class Meta:
+        ordering = ['livro_id']
+
+    def __str__(self):
+        return self.livro_nome #retorna o atributo nome
